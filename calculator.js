8a05c7c209dfaa73c80c15a9755d106a78a9aa64
nesradinabduramanomer <ammarnasradin@gmail.com>
2026-07-09 15:45:24 +0300

    Fix calculator

--- calculator.js
+++ calculator.js
@@ -1,23 +1,36 @@
 function add() {
-    let a = Number(document.getElementById("num1").value);
-    let b = Number(document.getElementById("num2").value);
-    document.getElementById("result").innerHTML = "Result: " + (a + b);
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = "Result: " + (num1 + num2);
 }
 
 function subtract() {
-    let a = Number(document.getElementById("num1").value);
-    let b = Number(document.getElementById("num2").value);
-    document.getElementById("result").innerHTML = "Result: " + (a - b);
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = "Result: " + (num1 - num2);
 }
 
 function multiply() {
-    let a = Number(document.getElementById("num1").value);
-    let b = Number(document.getElementById("num2").value);
-    document.getElementById("result").innerHTML = "Result: " + (a * b);
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = "Result: " + (num1 * num2);
 }
 
 function divide() {
-    let a = Number(document.getElementById("num1").value);
-    let b = Number(document.getElementById("num2").value);
-    document.getElementById("result").innerHTML = "Result: " + (a / b);
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    if (num2 == 0) {
+        document.getElementById("result").innerHTML = "0'n qooduun hin danda'amu!";
+    } else {
+        document.getElementById("result").innerHTML = "Result: " + (num1 / num2);
+    }
+}
+
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function showMessage() {
+    alert("Baga Nagaan Dhuftan! 😊");
 }
