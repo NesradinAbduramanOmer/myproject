a5ba608ba52c70983db163c599898ed82507b299
nesradinabduramanomer <ammarnasradin@gmail.com>
2026-07-09 21:54:39 +0300

    Add To-Do List page

---
+++ todo.js
@@ -0,0 +1,15 @@
+function addTask() {
+    let task = document.getElementById("task").value;
+
+    if (task === "") {
+        alert("Hojii tokko galchi!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    document.getElementById("list").appendChild(li);
+
+    document.getElementById("task").value = "";
+}
